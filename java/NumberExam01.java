bf693d4b1062b3b4afd8c4ef574730d13f5a3e14
Yubin0908 <dasol_b@kakao.com>
2023-06-27 22:33:01 +0900

    0627 22:32:00

---
+++ java/NumberExam01.java
@@ -0,0 +1,17 @@
+
+public class NumberExam01 {
+    // 타입별 최대 최소값
+    public static void main(String[] args) {
+        int maxInt = Integer.MAX_VALUE;
+        int minInt = Integer.MIN_VALUE;
+
+        double maxDouble = Double.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+
+        System.out.println(maxInt);
+        System.out.println(minInt);
+
+        System.out.println(maxDouble);
+        System.out.println(minDouble);
+    }
+}
