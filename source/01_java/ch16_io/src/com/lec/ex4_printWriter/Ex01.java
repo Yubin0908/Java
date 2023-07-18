a7f3f783ed5c355cfd09fda873237e9b16309e63
Yubin0908 <dasol_b@kakao.com>
2023-07-18 16:36:50 +0900

    I/O

---
+++ source/01_java/ch16_io/src/com/lec/ex4_printWriter/Ex01.java
@@ -0,0 +1,12 @@
+package com.lec.ex4_printWriter;
+// printWriter : OutputStream을 기본스트림으로 하는 보조스트림
+//	Writer를 기본스트림으로 하는 보조스트림
+// printWriter 혼자 기본스트림처럼 사용.
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
